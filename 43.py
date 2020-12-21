cf168a0e4849c38d2e6be0e8545ebe924dc0867f
e <65376762+hdhdhfhfirjf@users.noreply.github.com>
2020-12-21 08:57:17 -0500

    Multiply Strings

---
+++ 43.py
@@ -0,0 +1,15 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        number1 = 0
+        number2 = 0
+        for digit in num1:
+            number1 *= 10
+            for d in '0123456789':
+                number1 += digit > d
+
+        for digit in num2:
+            number2 *= 10
+            for d in '0123456789':
+                number2 += digit > d
+
+        return str(number1 * number2)
